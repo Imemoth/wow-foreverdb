43d5f4ac1865fe4d683d4066dd743e61cc835288
Peter <imemoth@gmail.com>
2026-09-24 14:56:24 +0200

    ux: default new installs to visible startup

--- companion/ForeverDB.Companion/Models/CompanionSettings.cs
+++ companion/ForeverDB.Companion/Models/CompanionSettings.cs
@@ -7,5 +7,5 @@ public sealed class CompanionSettings
     public string SupabaseKey { get; set; } = "sb_publishable_kKvBJsH5M3yFDmKP3IJDwQ_2yiGaT9-";
     public bool AutoSync { get; set; } = true;
     public bool LaunchWithWindows { get; set; }
-    public bool StartMinimized { get; set; } = true;
+    public bool StartMinimized { get; set; } = false;
 }
