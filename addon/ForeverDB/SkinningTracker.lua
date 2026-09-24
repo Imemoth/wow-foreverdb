26a249d29d6733034430c2e8678094e2ee1c0325
Peter <imemoth@gmail.com>
2026-09-24 10:07:14 +0200

    feat: classify mob, gathering, chest and quest loot

--- addon/ForeverDB/SkinningTracker.lua
+++ addon/ForeverDB/SkinningTracker.lua
@@ -2,7 +2,26 @@ local _, FDB = ...
 
 local skinFrame
 
-local SKINNING_SPELL_NAME = PROFESSIONS_SKINNING or "Skinning"
+local KNOWN_SKINNING_SPELLS = {
+    [8613] = true,
+    [8617] = true,
+    [8618] = true,
+    [10768] = true,
+}
+
+local function getSpellName(spellId)
+    if C_Spell and C_Spell.GetSpellName then
+        return C_Spell.GetSpellName(spellId)
+    end
+    if GetSpellInfo then
+        return GetSpellInfo(spellId)
+    end
+end
+
+function FDB:IsSkinningSpell(spellId)
+    if KNOWN_SKINNING_SPELLS[spellId] then return true end
+    return getSpellName(spellId) == "Skinning"
+end
 
 function FDB:InitializeSkinningTracker()
     if skinFrame then return end
@@ -11,16 +30,16 @@ function FDB:InitializeSkinningTracker()
     skinFrame:RegisterEvent("UNIT_SPELLCAST_SUCCEEDED")
 
     skinFrame:SetScript("OnEvent", function(_, _, unitTarget, _, spellId)
-        if unitTarget ~= "player" then return end
-
-        local spellName = C_Spell and C_Spell.GetSpellName and C_Spell.GetSpellName(spellId)
-        if not spellName and GetSpellInfo then
-            spellName = GetSpellInfo(spellId)
+        if unitTarget ~= "player" or not FDB:IsSkinningSpell(spellId) then
+            return
         end
 
-        if spellName == SKINNING_SPELL_NAME or spellName == "Skinning" then
-            FDB.ActiveLootMode = "skinning"
-            FDB:Debug("skinning detected")
-        end
+        local guid = UnitGUID("target")
+        local sourceType, sourceId = FDB:ParseSourceGuid(guid)
+
+        if sourceType ~= "creature" or not sourceId then return end
+
+        FDB:SetPendingGatheringKind("skinning", guid)
+        FDB:Debug("skinning detected for", sourceId)
     end)
 end
