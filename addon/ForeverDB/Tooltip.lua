5d6e2b465b506eb0a3a22287dd48d5dc6e98dc84
Peter <imemoth@gmail.com>
2026-09-24 09:18:38 +0200

    feat: add initial ForeverDB addon and data model

---
+++ addon/ForeverDB/Tooltip.lua
@@ -0,0 +1,7 @@
+local _, FDB = ...
+
+function FDB:InitializeTooltip()
+    -- Tooltip enrichment is intentionally deferred until the beta collector
+    -- proves that NPC identification and observation counting are reliable.
+    -- Keeping this module present makes the v0.1 file contract stable.
+end
