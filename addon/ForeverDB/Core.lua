a8ec01487bba6c17d5768c9dd2f5d0dbfdae3d80
Peter <imemoth@gmail.com>
2026-09-24 12:17:01 +0200

    feat: record zone subzone and map coordinates

--- addon/ForeverDB/Core.lua
+++ addon/ForeverDB/Core.lua
@@ -3,8 +3,8 @@ local addonName, FDB = ...
 FDB = FDB or {}
 _G[addonName] = FDB
 
-FDB.VERSION = "0.2.5-alpha"
-FDB.SCHEMA_VERSION = 6
+FDB.VERSION = "0.2.6-alpha"
+FDB.SCHEMA_VERSION = 7
 FDB.DEBUG = true
 
 local PREFIX = "|cff7dd3fcForeverDB|r"
